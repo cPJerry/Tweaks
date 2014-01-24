a60bc8c1c024a6bdc1ba2ea3cf1331bcd00a7358
Jerald Johnson <jerald@cpanel.net>
2014-01-23 20:40:04 -0600

    Fix includes to only run on the queue

--- classifyFromQueue.js
+++ classifyFromQueue.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name                Ticket Classify
-// @include             https://staff.cpanel.net/staff/*
-// @include             https://dec.cpanel.net/staff/*
-// @include             https://hex.cpanel.net/staff/*
-// @include             https://oct.cpanel.net/staff/*
+// @include             https://staff.cpanel.net/staff/
+// @include             https://staff.cpanel.net/staff/index.cgi*
+// @include             https://dec.cpanel.net/staff/
+// @include             https://dec.cpanel.net/staff/index.cgi*
+// @include             https://hex.cpanel.net/staff/
+// @include             https://hex.cpanel.net/staff/index.cgi*
+// @include             https://oct.cpanel.net/staff/
+// @include             https://oct.cpanel.net/staff/index.cgi*
 // ==/UserScript==
 function addClass(type2) {
     var pri = $('[id="'+type2+'"]');
