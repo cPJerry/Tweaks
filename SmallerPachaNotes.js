0d8093316ca41b29000e70744813c787e876879f
Jerald Johnson <jerald@cpanel.net>
2014-01-23 21:32:54 -0600

    Add ability to make Pacha notes smaller. Thanks David Nielsongit add SmallerPachaNotes.js

---
+++ SmallerPachaNotes.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name                Small Pacha Notes
+// @include             https://staff.cpanel.net/staff/msg.new.cgi?*
+// @include             https://dec.cpanel.net/staff/msg.new.cgi?*
+// @include             https://hex.cpanel.net/staff/msg.new.cgi?*
+// @include             https://oct.cpanel.net/staff/msg.new.cgi?*
+// ==/UserScript==
+$('.pacha_note_container').children('.internal_note_container').children('span').each(function() {
+	$(this).attr('style','float: left; width: 100%; word-wrap: break-word; ');
+});
+
