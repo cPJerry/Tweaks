b944675e1dc4feead928d236f069d3089bfeb0f2
Jerald Johnson <jerald@cpanel.net>
2014-01-24 22:41:07 -0600

    Add note support

--- hide_ticket_repsonse.js
+++ hide_ticket_repsonse.js
@@ -5,15 +5,13 @@
 // @include             https://hex.cpanel.net/staff/msg.new.cgi?*
 // @include             https://oct.cpanel.net/staff/msg.new.cgi?*
 // ==/UserScript==
-/*
-$('.pacha_note_container').children('.internal_note_container').children('span').each(function() {
-	$(this).attr('style','float: left; width: 100%; word-wrap: break-word; ');
-});
-*/
 $('.transactions_container').children('table').children('tbody').children('tr').children('.customer_response_container').parent().parent().children('.stblheader2').click(function() {
 	$(this).parent().children('tr').children('.customer_response_container').toggle();
 });
 
 $('.transactions_container').children('table').children('tbody').children('tr').children('.employee_response_container').parent().parent().children('.stblheader2').click(function() {
 	$(this).parent().children('tr').children('.employee_response_container').toggle();
+});
+$('.transactions_container').children('table').children('tbody').children('tr').children('.note_container').children('p').click(function() {
+    $(this).parent().children('div').toggle();
 });
