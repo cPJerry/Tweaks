0a4a8b21132d292ccfd9e1c2cfee487246e4b573
Jerald Johnson <jerald@cpanel.net>
2014-01-24 21:39:41 -0600

    Allows hiding reponses. Thanks Marco

---
+++ hide_ticket_repsonse.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name                Hide item
+// @include             https://staff.cpanel.net/staff/msg.new.cgi?*
+// @include             https://dec.cpanel.net/staff/msg.new.cgi?*
+// @include             https://hex.cpanel.net/staff/msg.new.cgi?*
+// @include             https://oct.cpanel.net/staff/msg.new.cgi?*
+// ==/UserScript==
+/*
+$('.pacha_note_container').children('.internal_note_container').children('span').each(function() {
+	$(this).attr('style','float: left; width: 100%; word-wrap: break-word; ');
+});
+*/
+$('.transactions_container').children('table').children('tbody').children('tr').children('.customer_response_container').parent().parent().click(function() {
+	$(this).children('tr').children('.customer_response_container').toggle();
+});
+
+$('.transactions_container').children('table').children('tbody').children('tr').children('.employee_response_container').parent().parent().click(function() {
+	$(this).children('tr').children('.employee_response_container').toggle();
+});
