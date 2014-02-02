a426759c1d62d28f4e616ab96124d39980fca80a
Jerald Johnson <jerald@cpanel.net>
2014-02-02 13:53:59 -0600

    Fixed bug where it doesn't work after ticket update

--- hide_ticket_repsonse.user.js
+++ hide_ticket_repsonse.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
-// @version		1.0.0.1
-// @updateurl		https://github.com/cPJerry/Tweaks/raw/master/hide_ticket_repsonse.user.js
+// @version             1.0.0.2
+// @updateurl           https://github.com/cPJerry/Tweaks/raw/master/hide_ticket_repsonse.user.js
 // @name                Hide item
 // @include             https://staff.cpanel.net/staff/msg.new.cgi?*
 // @include             https://dec.cpanel.net/staff/msg.new.cgi?*
 // @include             https://hex.cpanel.net/staff/msg.new.cgi?*
 // @include             https://oct.cpanel.net/staff/msg.new.cgi?*
 // ==/UserScript==
-$('.transactions_container').children('table').children('tbody').children('tr').children('.customer_response_container').parent().parent().children('.stblheader2').click(function() {
-	$(this).parent().children('tr').children('.customer_response_container').toggle();
-});
-
-$('.transactions_container').children('table').children('tbody').children('tr').children('.employee_response_container').parent().parent().children('.stblheader2').click(function() {
+$('body').on("click","tr .stblheader2",function(){
+	console.log("TEST");$(this).parent().children('tr').children('.customer_response_container').toggle();
+    });
+$('body').on("click","tr .stblheader2",function(){
 	$(this).parent().children('tr').children('.employee_response_container').toggle();
-});
-$('.transactions_container').children('table').children('tbody').children('tr').children('.note_container').children('p').click(function() {
-    $(this).parent().children('div').toggle();
-});
+    });
+$('body').on("click","td .note_container > p",function(){
+	$(this).parent().children('div .internal_note_container').toggle();
+    });
