182992857ebc90cc5f02d5ff5ed86e5d91278054
cPJerry <jerald@cpanel.net>
2015-01-22 05:26:08 -0600

    Update upvote.user.js
    This now runs for frontpage too. Upvote every comment in a gallery post

--- upvote.user.js
+++ upvote.user.js
@@ -3,17 +3,10 @@
 // @version      0.1
 // @author       Jerald Johnson
 // @match        http://imgur.com/user/*
+// @match		 http://imgur.com/gallery/*
 // ==/UserScript==
 
-if ($('#captions').children('div').children('h2').html() == "Gallery comments") {
-    function upvote() {
-        $('div[class="arrows"]').find("div[title='like']").each(function() {
-                if (!$(this).hasClass("pushed")) {
-                    $(this).children("span").click();
-                }
-            }
-     	);
-    }                        
+if ($('#captions').children('div').children('h2').html() == "Gallery comments") {                      
     var html = "&nbsp; <a id='upvotefairy' href='javascript:void;'>Upvote Fairy</a>";
     $('#captions').children('div').children('h2').html($('#captions').children('div').children('h2').html()+html);
     $('#upvotefairy').click(function() {
@@ -28,3 +21,11 @@ if ($('#captions').children('div').children('h2').html() == "Gallery comments") 
 
 
 
+
+$('#mainUpArrow').children('span').click(function() {
+        $('div[class="arrows"]').find("div[title='like']").each(function() {
+            if (!$(this).hasClass("pushed")) {
+                $(this).children("span").click();
+            }
+        });
+    });
